a975a26aa0ef4e1604acc594a8bbcd85322f9ea2
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-02-28 01:00:07 -0800

    build: cleanup old data on bitstream generating

--- tcl/run_bitstream.tcl
+++ tcl/run_bitstream.tcl
@@ -5,12 +5,26 @@ set build_dir [lindex $argv 1]
 # open the project
 open_project $build_dir/$project_name.xpr
 
-# launch synthesis and implementation
+# clear out old failed data
+reset_run synth_1
+reset_run impl_1
+
+# launch synthesis
 launch_runs synth_1 -jobs 4
 wait_on_run synth_1
 
+# check if synthesis actually succeeded before moving to implementation
+if {[get_property PROGRESS [get_runs synth_1]] != "100%"} {
+   error "ERROR: Synthesis failed!"
+}
+
+# launch implementation and bitstream
 launch_runs impl_1 -to_step write_bitstream -jobs 4
 wait_on_run impl_1
 
-# save and close
+# Check if implementation succeeded
+if {[get_property PROGRESS [get_runs impl_1]] != "100%"} {
+   error "ERROR: Implementation/Bitstream failed! Check the Vivado log in .build/."
+}
+
 close_project
