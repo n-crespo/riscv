80d452efa68f00d9a9e6ee8e31b10713672111bd
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-03-01 14:36:57 -0800

    feat: update immediate generator to support s type instructions

--- hardware/src/imm_gen.v
+++ hardware/src/imm_gen.v
@@ -1,11 +1,25 @@
 // Generates sign-extended immediate values from a 32-bit instruction
 module imm_gen (
-    input  [31:0] instr,
-    output [31:0] imm_out
+    input      [31:0] instr,
+    output reg [31:0] imm_out
 );
 
-  // right now we only support I-type instructions (like addi)
-  // this takes bits 31:20 and copies the sign bit (bit 31) 20 times to fill the 32-bit width
-  assign imm_out = {{20{instr[31]}}, instr[31:20]};
+  wire [6:0] opcode = instr[6:0];
+
+  always @(*) begin
+    case (opcode)
+      // i-type instructions (addi, lw, etc)
+      7'b0010011, 7'b0000011: begin
+        imm_out = {{20{instr[31]}}, instr[31:20]};
+      end
+
+      // s-type instructions (sw, etc)
+      7'b0100011: begin
+        imm_out = {{20{instr[31]}}, instr[31:25], instr[11:7]};
+      end
+
+      default: imm_out = 32'd0;
+    endcase
+  end
 
 endmodule
