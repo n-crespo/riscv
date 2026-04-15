f1d812f18e32299bfeadd64ca6acb44881caf8fa
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-04-14 23:57:23 -0700

    feat(data_mem): initialize RAM to zero at sim start

--- src/rtl/data_mem.v
+++ src/rtl/data_mem.v
@@ -1,18 +1,26 @@
 `timescale 1ns / 1ps
 
 /**
- * Data memory module with asynchronous read for a 2-stage pipeline
+ * Data memory module with zero-initialization for simulation
  */
 module data_mem (
     input clk,
-    input  [ 3:0] be,    // byte enable mask (one bit per byte)
-    input  [11:0] addr,  // expanded to 12 bits for 4096 words (16 kb)
+    input  [ 3:0] be,    // byte enable mask
+    input  [11:0] addr,  // 12-bit index for 4096 words (16 kb)
     input  [31:0] wd,    // data to write (pre-steered in top.v)
     output [31:0] rd     // raw 32-bit data out
 );
 
   // internal storage for 4096 32-bit words
   reg [31:0] ram[0:4095];
+
+  // loop through all memory addresses to clear them at t=0 (only in simulation)
+  integer i;
+  initial begin
+    for (i = 0; i < 4096; i = i + 1) begin
+      ram[i] = 32'h0;
+    end
+  end
 
   // synchronous write logic with byte-level granularity
   always @(posedge clk) begin
@@ -23,7 +31,7 @@ module data_mem (
     if (be[3]) ram[addr][31:24] <= wd[31:24];
   end
 
-  // asynchronous read used for immediate feedback in the execute stage
+  // asynchronous read for immediate feedback in the execute stage
   assign rd = ram[addr];
 
 endmodule
