943b035caa9beab1fa0366dbd4c4eec85b75a5c5
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-04-14 23:46:54 -0700

    feat(instr_mem): increase allocated memory for storing instructions

--- src/rtl/instr_mem.v
+++ src/rtl/instr_mem.v
@@ -1,28 +1,32 @@
 `timescale 1ns / 1ps
-// dual port memory module optimized for BRAM
+
+/**
+ * Dual-port memory module optimized for bram-like behavior
+ */
 module instr_mem (
     input clk,
 
-    // port a: for loading instructions via uart
-    input        we,
-    input [ 7:0] addr_a,
-    input [31:0] din_a,
+    // used by UART snooper to live-program FPGA
+    input        we,      // write enable
+    input [11:0] addr_a,  // index where instruction is to be saved
+    input [31:0] din_a,   // the actual instruction to be loaded
 
-    // port b: for the cpu to read instructions
-    input [31:0] addr_b,
-    output reg [31:0] dout_b  // synchronous read
+    // used by CPU to read instructions
+    input [31:0] addr_b,  // address that the PC wants to read from
+    output reg [31:0] dout_b  // the instruction we are going to return
 );
 
-  reg [31:0] ram[0:255];
+  // 16 kb of instruction space (4096 32-bit words)
+  reg [31:0] ram[0:4095];
 
-  // write logic (port a)
+  // synchronous write logic for loading program data via uart
   always @(posedge clk) begin
     if (we) ram[addr_a] <= din_a;
   end
 
-  // read logic (port b) - synchronous read (1-cycle delay)
+  // bits [13:2] divides requested address by 4 to get proper RAM address
   always @(posedge clk) begin
-    dout_b <= ram[addr_b[9:2]];
+    dout_b <= ram[addr_b[13:2]];
   end
 
 endmodule
